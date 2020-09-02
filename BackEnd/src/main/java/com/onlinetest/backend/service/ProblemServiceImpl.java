0ae6c6402353344cc9ecb35b94332019341ee04f
khJoo5 <delight_joo@naver.com>
2020-09-02 14:23:24 +0900

    [BE-002] 소속기관 추가

--- BackEnd/src/main/java/com/onlinetest/backend/service/ProblemServiceImpl.java
+++ BackEnd/src/main/java/com/onlinetest/backend/service/ProblemServiceImpl.java
@@ -1,8 +1,20 @@
 package com.onlinetest.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.onlinetest.backend.dao.ProblemDaoImpl;
 
 @Service
 public class ProblemServiceImpl implements IProblemService{
+	
+	@Autowired
+	private ProblemDaoImpl problemdao;
+
+	@Override
+	public void agency(String name) {
+		problemdao.setName(name);
+		
+	}
 
 }
