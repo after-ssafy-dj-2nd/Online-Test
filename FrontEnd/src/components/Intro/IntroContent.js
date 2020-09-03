fd05324823ffd38722f1b55eabe9563724471356
kimjunyoung <kimjy392@gmail.com>
2020-09-04 05:39:58 +0900

    [FE-012] IntroPage 기본 레이아웃 설정

---
+++ FrontEnd/src/components/Intro/IntroContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './IntroDefault.css'
+const IntroContent = () => {
+    return (
+        <div className="intro-content">
+            Intro Contents
+        </div>
+    );
+};
+
+export default IntroContent;
