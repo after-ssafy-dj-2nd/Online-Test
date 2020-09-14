28a16a80cf5926aa502b8a4a850d02dd644c8dc7
khJoo5 <delight_joo@naver.com>
2020-09-14 16:59:48 +0900

    [BE-003] 회원가입, 로그인 수정

--- BackEnd/src/main/java/com/onlinetest/backend/dto/User.java
+++ BackEnd/src/main/java/com/onlinetest/backend/dto/User.java
@@ -5,7 +5,8 @@ public class User {
 	private String user_id;
 	private String password;
 	private int auth;
-	private int agency_id;
+	private String name;
+	private String email;
 	
 	public User() {
 		super();
@@ -18,21 +19,31 @@ public class User {
 		this.password = password;
 	}
 
-	public User(String user_id, String password, int auth, int agency_id) {
+	public User(String user_id, String password, String name, String email) {
+		super();
+		this.user_id = user_id;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
+	public User(String user_id, String password, int auth, String name, String email) {
 		super();
 		this.user_id = user_id;
 		this.password = password;
 		this.auth = auth;
-		this.agency_id = agency_id;
+		this.name = name;
+		this.email = email;
 	}
 
-	public User(int id, String user_id, String password, int auth, int agency_id) {
+	public User(int id, String user_id, String password, int auth, String name, String email) {
 		super();
 		this.id = id;
 		this.user_id = user_id;
 		this.password = password;
 		this.auth = auth;
-		this.agency_id = agency_id;
+		this.name = name;
+		this.email = email;
 	}
 
 	public int getId() {
@@ -67,15 +78,20 @@ public class User {
 		this.auth = auth;
 	}
 
-	public int getAgency_id() {
-		return agency_id;
+	public String getName() {
+		return name;
 	}
 
-	public void setAgency_id(int agency_id) {
-		this.agency_id = agency_id;
+	public void setName(String name) {
+		this.name = name;
 	}
-	
-	
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
