e10fefc34af078b0625bcec2dd23d3521d999e5d
jr31112 <jr435@naver.com>
2020-09-16 15:42:53 +0900

    [BE-013] 보기 CRUD 및 application.properties 삭제

---
+++ BackEnd/src/main/java/com/onlinetest/backend/dto/Example.java
@@ -0,0 +1,56 @@
+package com.onlinetest.backend.dto;
+
+import java.util.List;
+
+public class Example {
+    private int id;
+    private int question_id;
+    private String content;
+    private Boolean correct;
+
+    public Example() {
+        super();
+    }
+
+    public Example(int id, int question_id, String content, Boolean correct) {
+        super();
+        this.id = id;
+        this.question_id = question_id;
+        this.content = content;
+        this.correct = correct;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(int question_id) {
+        this.question_id = question_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(Boolean correct) {
+        this.correct = correct;
+    }
+}
+
+
