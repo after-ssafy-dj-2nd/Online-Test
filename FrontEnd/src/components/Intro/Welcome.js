21978cba0595f91c67a0c18c5d701108b47ee239
kimjunyoung <kimjy392@gmail.com>
2020-09-21 14:18:48 +0900

    [FE-013] 인트로페이지 버튼 클릭시 이벤트 추가

--- FrontEnd/src/components/Intro/Welcome.js
+++ FrontEnd/src/components/Intro/Welcome.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './IntroDefault.css';
 import './Welcome.css';
+import { $ } from '../../util/DOM'
+import { Link } from 'react-router-dom'
 const Welcome = () => {
+  
+  const goContent = () => {
+    window.scrollTo({
+      top: $('#intorduceContent').offsetTop, 
+      behavior : 'smooth'
+    })
+  }
   return (
     <div className="intro-content">
       <div className="welcome-wrap">
         <div className="welcome-contents">
           <div className="project-name">
             <h1>프로젝트 이름 및 간단한 소개</h1>
-            <button>시작하기</button>
+            <button><Link to="/login">시작하기</Link></button>
           </div>
           <div className="welcome-image">이미지 넣을 자리</div>
         </div>
         <div className="welcome-button">
-          <button className="down-button">Down</button>
+          <button onClick={goContent} className="down-button">Down</button>
         </div>
       </div>
   </div>
