c15e973d15be9b43505be9c3744038b9619395b3
khJoo5 <delight_joo@naver.com>
2020-09-21 14:50:57 +0900

    [BE-014] JWT 추가

---
+++ BackEnd/src/main/java/com/onlinetest/backend/interceptor/WebConfig.java
@@ -0,0 +1,23 @@
+package com.onlinetest.backend.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private static final String[] EXCLUDE_PATHS = {
+            "/api/login"
+    };
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtInterceptor)
+						.addPathPatterns("/api/**")
+						.excludePathPatterns(EXCLUDE_PATHS);
+    }
+}
---
+++ BackEnd/src/main/java/com/onlinetest/backend/service/JwtServiceImpl.java
@@ -0,0 +1,89 @@
+package com.onlinetest.backend.service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.onlinetest.backend.interceptor.UnauthorizedException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service("jwtService")
+public class JwtServiceImpl implements IJwtService{
+
+	private static final String SALT =  "secretKey";
+	public static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
+	
+	@Override
+	public <T> String create(String key, T data, String subject){
+		String jwt = Jwts.builder()
+						 .setHeaderParam("typ", "JWT")
+						 .setHeaderParam("regDate", System.currentTimeMillis())
+						 .setSubject(subject)
+						 .claim(key, data)
+						 .signWith(SignatureAlgorithm.HS256, this.generateKey())
+						 .compact();
+		return jwt;
+	}	
+
+	private byte[] generateKey(){
+		byte[] key = null;
+		try {
+			key = SALT.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			if(logger.isInfoEnabled()){
+				e.printStackTrace();
+			}else{
+				logger.error("Making JWT Key Error ::: {}", e.getMessage());
+			}
+		}
+		
+		return key;
+	}
+	
+	@Override
+	public boolean isUsable(String jwt) {
+		try{
+			Jws<Claims> claims = Jwts.parser()
+					  .setSigningKey(this.generateKey())
+					  .parseClaimsJws(jwt);
+			return true;
+			
+		}catch (Exception e) {
+			throw new UnauthorizedException();
+		}
+	}
+	
+	@Override
+	public Map<String, Object> get(String key) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		String jwt = request.getHeader("Authorization");
+		Jws<Claims> claims = null;
+		try {
+			claims = Jwts.parser()
+						 .setSigningKey(SALT.getBytes("UTF-8"))
+						 .parseClaimsJws(jwt);
+		} catch (Exception e) {
+			throw new UnauthorizedException();
+		}
+		@SuppressWarnings("unchecked")
+		Map<String, Object> value = (LinkedHashMap<String, Object>)claims.getBody().get(key);
+		return value;
+	}
+	
+	@Override
+	public int getMemberId() {
+		return (int)this.get("user").get("memberId");
+	}
+}
